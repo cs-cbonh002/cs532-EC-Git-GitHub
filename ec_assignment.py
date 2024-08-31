57701c59bfcb037057d491eb001e498b7fde42ff
cs-cbonh002 <bonham_36@hotmail.com>
2024-08-31 15:23:41 -0400

    Update ec_assignment.py with additional string and print statment.

--- ec_assignment.py
+++ ec_assignment.py
@@ -1,2 +1,5 @@
 example_string = "This is an example string"
 print(example_string)
+
+example_string2 = "This is the change that I am making to this file for the assignment."
+print(example_string2)
